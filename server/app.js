71582dc819a4a3b838a289892613b6f1bbf2fcb0
trip <fuad47722@gmail.com>
2025-04-10 17:59:24 +0300

    refactor: comment out unused code in admin and app routes for clarity

--- server/app.js
+++ server/app.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const cors = require('cors')
-const {admin} = require('./Route/admin')
+// const express = require('express')
+// const cors = require('cors')
+// const {admin} = require('./Route/admin')
 
 
 
-const app = express()
+// const app = express()
 
 
 
-app.use(cors({
-    origin: 'http://localhost:5174',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5174',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
+//     credentials: true
+// }))
 
-app.use(express.json())
-app.use('/admin', admin);
-
-
+// app.use(express.json())
+// app.use('/admin', admin);
 
 
-app.listen(3032 , ()=> {
-    console.log('server is listn on port 3032')
-})
+
+
+// app.listen(3032 , ()=> {
+//     console.log('server is listn on port 3032')
+// })
