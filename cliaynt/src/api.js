912cde3f6a2e90c5040735051eb8191d860a2214
Fuad Jemal <fuad47722@gmail.com>
2025-07-06 09:08:29 +0300

    fix: update API base URL for development and production environments; add vercel configuration for rewrites

--- cliaynt/src/api.js
+++ cliaynt/src/api.js
@@ -1,12 +1,14 @@
 
 import axios, { CanceledError } from "axios";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 const api = axios.create({
-    baseURL: "http://localhost:3032",
+    baseURL: isDevelopment ? `http://localhost:4000` : 'https://jejan.selamdca.org/',
     withCredentials: true,
 });
 
-const nPoint = "http://localhost:3032";
+const nPoint = `http://localhost:4000`;
 
 export default api;
 export { CanceledError, nPoint };
