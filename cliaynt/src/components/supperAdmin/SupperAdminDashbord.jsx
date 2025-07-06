4e9cdfae6ab45211e593693e32b9a09850b208c9
Fuad Jemal <fuad47722@gmail.com>
2025-07-06 11:36:41 +0300

    feat: add role selection to admin form in SupperAdminDashbord component

--- cliaynt/src/components/supperAdmin/SupperAdminDashbord.jsx
+++ cliaynt/src/components/supperAdmin/SupperAdminDashbord.jsx
@@ -18,7 +18,8 @@ function SupperAdminDashbord() {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        password: ''
+        password: '',
+        role: ''
     });
 
     useEffect(() => {
@@ -47,7 +48,8 @@ function SupperAdminDashbord() {
         setFormData({
             name: '',
             email: '',
-            password: ''
+            password: '',
+            role: ''
         });
         setShowModal(true);
     };
@@ -276,6 +278,22 @@ function SupperAdminDashbord() {
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                                     placeholder="Enter admin email"
+                                />
+                            </div>
+
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">Role</label>
+                                <select type="role"
+                                    name="Role"
+                                    value={formData.role}
+                                    onChange={handleChange}
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                    placeholder="Enter admin role">
+                                    <option>ADMIN</option>
+                                    <option>SUPPERADMIN</option>
+                                </select>
+                                <input
+
                                 />
                             </div>
 
