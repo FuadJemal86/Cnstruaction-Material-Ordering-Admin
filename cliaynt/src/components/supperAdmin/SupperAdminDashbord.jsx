0803a97c9ed5182280a21633899d221317081eb2
Fuad Jemal <fuad47722@gmail.com>
2025-07-06 11:42:41 +0300

    fix: admin roles

--- cliaynt/src/components/supperAdmin/SupperAdminDashbord.jsx
+++ cliaynt/src/components/supperAdmin/SupperAdminDashbord.jsx
@@ -284,17 +284,15 @@ function SupperAdminDashbord() {
                             <div>
                                 <label className="block text-sm font-medium text-gray-700 mb-1">Role</label>
                                 <select type="role"
-                                    name="Role"
+                                    name="role"
                                     value={formData.role}
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                                     placeholder="Enter admin role">
-                                    <option>ADMIN</option>
-                                    <option>SUPPERADMIN</option>
+                                    <option value={'ADMIN'}>ADMIN</option>
+                                    <option value={'SUPPERADMIN'}>SUPPERADMIN</option>
                                 </select>
-                                <input
 
-                                />
                             </div>
 
                             {
